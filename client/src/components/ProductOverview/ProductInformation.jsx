c9d18bb37a4641a567dd06cc69e8377f03dd140f
boo radley <booradley@boos-MacBook-Air.local>
2021-12-22 15:05:18 -0800

    stars for product info sidebar

--- client/src/components/ProductOverview/ProductInformation.jsx
+++ client/src/components/ProductOverview/ProductInformation.jsx
@@ -1,7 +1,8 @@
 // import React, { useState } from 'react';
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Stars from '../Stars';
+import calcAvgRating from '../helpers/calcAvgRating';
 
 function ProductInformation(props) {
   const {
@@ -14,11 +15,35 @@ function ProductInformation(props) {
   // console.log('product', product);
   // console.log('productStyleSelected', productStyleSelected);
 
+  const showStars = Object.keys(productRatings.ratings).length !== 0;
+  const productReviewsID = showStars ? 'product-reviews' : 'product-reviews-no-stars';
 
+  const origPrice = productStyleSelected.original_price;
+  const salePrice = productStyleSelected.sale_price;
 
   return (
     <div id="product-information" className="product-right-component">
-
+      <div id="product-rating">
+        { showStars
+        && <Stars rating={calcAvgRating(productRatings.ratings)} id={`DH-${product.id}`} />}
+        <a id={productReviewsID} href="#ratings-reviews-section">
+          {productReviews.results === undefined || productReviews.results.length === 0
+            ? null
+            : `Read all ${productReviews.results.length} reviews`}
+        </a>
+      </div>
+      <div id="product-category">{product.category.toUpperCase()}</div>
+      <div id="product-name"><b>{product.name}</b></div>
+      <div id="product-price-main">
+        {salePrice
+          ? (
+            <>
+              <span className="sale-price">{`$${salePrice} `}</span>
+              <span className="orig-price-strike">{`$${origPrice}`}</span>
+            </>
+          )
+          : <span>{` $${origPrice}`}</span>}
+      </div>
     </div>
   );
 }
